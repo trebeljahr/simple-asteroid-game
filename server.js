b46af9044a3e202384b6a40f1ac2c09ca5e41e38
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-11-23 16:44:02 +0100

    Rename public -> docs for github pages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = require("express")();
 let http = require("http").createServer(app);
 
-app.use(express.static("public"));
+app.use(express.static("docs"));
 
 http.listen(3000, function() {
   console.log("listening on *:3000");
