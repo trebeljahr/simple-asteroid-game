2738578c4a85a542c4392734407d74f13861fcfe
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2019-11-23 16:46:28 +0100

    Rerename docs to public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = require("express")();
 let http = require("http").createServer(app);
 
-app.use(express.static("docs"));
+app.use(express.static("public"));
 
 http.listen(3000, function() {
   console.log("listening on *:3000");
